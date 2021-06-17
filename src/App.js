6a980d5a7fe577d55ac5434b669ebb7cf4c8d1f9
Joe <jmwinitsky@gmail.com>
2021-06-17 11:48:57 -0700

    adding (blank) pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/events/:id" component={SingleEvent} />
+          <Route exact path="/new" component={NewEvent} />
+          <Route component={NoMatch} />
+        </Switch>
+        <Footer />
+    </Router>
   );
 }
 
