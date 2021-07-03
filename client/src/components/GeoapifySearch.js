733d585aaf75963940ddc50a980892a465a62b07
Joe <jmwinitsky@gmail.com>
2021-07-03 14:48:06 -0700

    geocoder autocomplete

---
+++ client/src/components/GeoapifySearch.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { GeoapifyGeocoderAutocomplete, GeoapifyContext } from '@geoapify/react-geocoder-autocomplete'
+import '@geoapify/geocoder-autocomplete/styles/minimal.css'
+
+function GeoapifySearch() {
+    function onPlaceSelect(value) {
+        console.log(value);
+      }
+    
+      function onSuggectionChange(value) {
+        console.log(value);
+      }
+    
+      function preprocessHook(value) {
+        return `${value}, Munich, Germany`
+      }
+    
+      function postprocessHook(feature) {
+        return feature.properties.street;
+      }
+    
+      function suggestionsFilter(suggestions) {
+        const processedStreets = [];
+    
+        const filtered = suggestions.filter(value => {
+          if (!value.properties.street || processedStreets.indexOf(value.properties.street) >= 0) {
+            return false;
+          } else {
+            processedStreets.push(value.properties.street);
+            return true;
+          }
+        })
+    
+        return filtered;
+      }
+    
+      return <GeoapifyContext apiKey="8220c7bc37d549848ae86fd9d8324474">
+    
+          <GeoapifyGeocoderAutocomplete
+            placeSelect={onPlaceSelect}
+            suggestionsChange={onSuggectionChange}
+          />
+        </GeoapifyContext>
+    }
+    
+    export default GeoapifySearch
--- client/src/pages/CreateEvent.js
+++ client/src/pages/CreateEvent.js
@@ -2,13 +2,17 @@ import React, { useState, useContext } from 'react'
 import API from '../utils/API';
 import GlobalContext from '../utils/GlobalState';
 import DateTimePicker from 'react-datetime-picker'
+import GeoapifySearch from '../components/GeoapifySearch';
 
 function CreateEvent() {
     const [formObject, setFormObject] = useState({})
     const [startDate, setStartDate] = useState(new Date())
     const [endDate, setEndDate] = useState(new Date())
+    const [venue, setVenue] = useState({})
     const { id } = useContext(GlobalContext);
     console.log(id)
+
+    
 
     function handleInputChange(event) {
         const { name, value } = event.target;
@@ -18,12 +22,14 @@ function CreateEvent() {
     function handleFormSubmit(event) {
         event.preventDefault();
         if (formObject.title && formObject.description) {
+          console.log(venue)
           API.saveEvent({
             title: formObject.title,
             description: formObject.description,
             start_date: startDate.toLocaleDateString(),
             end_date: endDate.toLocaleDateString(),
-            owner: id
+            owner: id,
+            venue: venue
           })
             .catch(err => console.log(err));
         }
@@ -36,6 +42,7 @@ function CreateEvent() {
                 <textarea onChange={handleInputChange} placeholder="event description" name="description" />
                 <DateTimePicker value={startDate} onChange={setStartDate} name="start_date" />
                 <DateTimePicker value={endDate} onChange={setEndDate} name="end_date" />
+                <GeoapifySearch type="text" onChange={setVenue}/>
                 <button type="submit" onClick={handleFormSubmit}>submit event</button>
             </form>
         </div>
