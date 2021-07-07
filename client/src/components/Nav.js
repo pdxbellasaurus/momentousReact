4ffc26a7ab7279cbf954b5a7919ec6af63c4cf3f
Joe <jmwinitsky@gmail.com>
2021-07-06 21:51:34 -0700

    about us and profile link

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -26,7 +26,7 @@ function Nav() {
         <div className="navbar-brand">
         <Link className="navbar-item" to="/">Home</Link>
         <Link className="navbar-item" to="/about">About</Link>
-        <Link className="navbar-item" to="/users/:id">Profile</Link>
+        <Link className="navbar-item" to="/profile">Profile</Link>
         <Link className="navbar-item" to="/new">Create Event</Link>
         <Link className="navbar-item" to="/login">Login</Link>
         <Link className="navbar-item" to="/signup">Signup</Link>
