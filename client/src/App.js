76b72dc3d25c00e63c8b755e608004437f34997e
pdxbellasaurus <carrieannyoung@gmail.com>
2021-07-08 20:13:17 -0700

    rte

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import React, { useState } from 'react'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './pages/Home';
-import About from './pages/About';
-import SingleEvent from './pages/SingleEvent';
+// import About from './pages/About';
+// import SingleEvent from './pages/SingleEvent';
 // import CreateEvent from './pages/CreateEvent';
-import NoMatch from './pages/NoMatch';
+// import NoMatch from './pages/NoMatch';
 import Nav from './components/Nav';
 // import Footer from './components/Footer';
 // import Profile from './pages/Profile';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
+// import Login from './pages/Login';
+// import SignUp from './pages/SignUp';
 import 'bulma/css/bulma.css';
 import GlobalContext from './utils/GlobalState';
 // import ProtectedRoute from './components/ProtectedRoute';
@@ -35,14 +35,14 @@ function App() {
         <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/events/:id" component={SingleEvent} />
+          {/* <Route exact path="/home" component={Home} />
+          <Route exact path="/about" component={About} /> */}
+          {/* <Route exact path="/events/:id" component={SingleEvent} /> */}
           {/* <ProtectedRoute exact path="/new" component={CreateEvent} />
           <ProtectedRoute exact path="/profile" component={Profile} /> */}
-          <Route exact path="/login" component={Login} />
+          {/* <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
-          <Route component={NoMatch} />
+          <Route component={NoMatch} /> */}
         </Switch>
         {/* <Footer /> */}
         </div>
